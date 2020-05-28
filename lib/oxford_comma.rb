fb70c320e2c195ab24d7c6010bef7255fc6b60db
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 20:11:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array[0]
+    elsif array.length == 2 
+    array[0] + " and " + array[1]
+  else array[-1] = "and " + array[-1]
+    array.join(", ")
+  end 
 end
