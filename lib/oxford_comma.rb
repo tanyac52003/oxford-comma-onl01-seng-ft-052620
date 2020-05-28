d66326f45e6988618e6e44dbce06c22085ea8191
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 20:09:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
     elsif array.length == 2 
     array[0] + " and " + array[1]
   else array[-1] = " and " + array[-1]
-    array.join(",")
+    array.join(" , ")
   end 
 end
