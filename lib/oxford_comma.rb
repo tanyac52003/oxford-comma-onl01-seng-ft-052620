57a4215df0cd50121ed70a6d17641bbf51c4b420
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 20:07:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array[0]
     elsif array.length == 2 
     array[0] + " and " + array[1]
-  else array[-1] = "and " + array[-1]
+  else array[-1] = " and " + array[-1]
     array.join(",")
   end 
 end
